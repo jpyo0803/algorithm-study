272b2495ded0853d69bff9e4b4882f64e728e3cf
Jinwon Pyo <gch99517@gmail.com>
2020-01-11 16:26:29 +0900

    Add insertion sort & merge sort algorithm

---
+++ src/insertion_sort.cpp
@@ -0,0 +1,54 @@
+/*
+  Insersion sort
+  O(n^2)
+  key iterates thru the array from 1 to n-1, everytime it relocates the key into its right place
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+
+using namespace std;
+
+void arr_print(const vector<int>& v) {
+  for (auto it = v.begin(); it != v.end(); it++) {
+    cout << *it << " ";
+  }
+  cout << endl;
+}
+
+int n = 10;
+vector<int> arr;
+
+void solve();
+
+int main() {
+  srand((unsigned int) time(NULL));
+
+  for (int i = 0; i < n; i++) {
+    arr.push_back((rand() % 50) - 25);
+  }
+
+  cout << "before sorted: \n";
+  arr_print(arr);
+  solve();
+  cout << "\nafter sorted: \n";
+  arr_print(arr);
+
+  return 0;
+}
+
+void solve() {
+  for (int i = 1; i < arr.size(); i++) {
+    int key = arr[i];
+    for (int j = i-1; j >= -1; j--) {
+      if (j >= 0 && key < arr[j]) {
+        arr[j+1] = arr[j];
+      } else {
+        arr[j+1] = key;
+        break;
+      }
+    }
+  }
+}
---
+++ src/merge_sort.cpp
@@ -0,0 +1,101 @@
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void arr_print(const vector<int>& v) {
+  for (auto it = v.begin(); it != v.end(); it++) {
+    cout << *it << " ";
+  }
+  cout << endl;
+}
+
+int n = 1000;
+vector<int> arr;
+
+void merge(int left, int right) {
+  int mid = (left + right) / 2;
+  int L1 = mid - left + 1;
+  int L2 = right - mid;
+  queue<int> v1;
+  queue<int> v2;
+
+  for (int i = 0; i < L1; i++) {
+    v1.push(arr[left + i]);
+  }
+  for (int i = 0; i < L2; i++) {
+    v2.push(arr[mid + 1 + i]);
+  }
+
+  int kp = left;
+
+  while (!v1.empty() && !v2.empty()) {
+    if (v1.front() < v2.front()) {
+      arr[kp] = v1.front();
+      v1.pop();
+    } else {
+      arr[kp] = v2.front();
+      v2.pop();
+    }
+    kp++;
+  }
+
+  while (!v1.empty()) {
+    arr[kp] = v1.front();
+    v1.pop();
+    kp++;
+  }
+  while (!v2.empty()) {
+    arr[kp] = v2.front();
+    v2.pop();
+    kp++;
+  }
+}
+void mergeSort(int left, int right) {
+  if (left == right) {  // if same
+    // nop
+  } else {
+    int mid = (left + right) / 2;
+    mergeSort(left, mid);
+    mergeSort(mid + 1, right);
+    merge(left, right);
+  }
+}
+
+int main() {
+  srand((unsigned int)time(NULL));
+
+  for (int i = 0; i < n; i++) {
+    arr.push_back((rand() % 10000) - 5000);
+  }
+
+  cout << "before sorted: \n";
+  arr_print(arr);
+  mergeSort(0, n - 1);
+  cout << "\nafter sorted: \n";
+  arr_print(arr);
+
+  vector<int> tmp;
+  for (int i = 0; i < arr.size(); i++) {
+    tmp.push_back(arr[i]);
+  }
+
+  sort(arr.begin(), arr.end());
+
+  bool same = true;
+  for (int i = 0; i < arr.size(); i++) {
+    if (tmp[i] != arr[i]) {
+      same = false;
+      break;
+    }
+  }
+  if (same) cout << "match!\n";
+  else cout << "Not match!\n";
+
+
+  return 0;
+}
