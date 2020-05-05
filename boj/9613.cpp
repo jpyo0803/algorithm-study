076fde42279d5a0dcc51fe7acffe6a77cd365d55
Jinwon Pyo <gch99517@gmail.com>
2020-05-05 18:17:18 +0900

    Add solution to boj 9613

---
+++ boj/9613.cpp
@@ -0,0 +1,42 @@
+//
+// Sum of GCD
+// Problem: https://www.acmicpc.net/problem/9613
+//
+
+
+#include <iostream>
+
+using namespace std;
+int T, N;
+int nums[100];
+
+int findGCD(int x, int y) {
+    int larger = (x > y) ? x : y;
+    int smaller = (larger == x) ? y : x;
+
+    int rem = larger % smaller;
+    while (rem != 0) {
+        larger = smaller;
+        smaller = rem;
+        rem = larger % smaller;
+    }
+    return smaller;
+}
+
+int main() {
+    cin >> T;
+    for (int tc = 0; tc < T; tc++) {
+        cin >> N;
+        for (int i = 0; i < N; i++) {
+            cin >> nums[i];
+        }
+        int64_t ans = 0;
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = i + 1; j < N; j++) {
+                ans += findGCD(nums[i], nums[j]);
+            }
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
