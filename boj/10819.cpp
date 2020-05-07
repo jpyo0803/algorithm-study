011fd15efc373141f978b1fe6d592e486b6c8059
Jinwon Pyo <gch99517@gmail.com>
2020-05-08 01:40:22 +0900

    Add solution to boj 10819

---
+++ boj/10819.cpp
@@ -0,0 +1,48 @@
+//
+// Make the difference largest
+// Problem: https://www.acmicpc.net/problem/10819
+//
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+const int MAX_SIZE{8};
+int N;
+int nums[MAX_SIZE];
+int chosen[MAX_SIZE];
+bool used[MAX_SIZE];
+
+int ans = 0;
+void perm(int num_chosen) {
+    if (num_chosen == N) {
+        int candidate = 0;
+        for (int i = 0; i < N - 1; i++) {
+            candidate += abs(nums[chosen[i]] - nums[chosen[i + 1]]);
+        }
+        if (ans < candidate) {
+            ans = candidate;
+        }
+        return;
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (!used[i]) {
+            used[i] = true;
+            chosen[num_chosen] = i;
+            perm(num_chosen + 1);
+            used[i] = false;
+        }
+    }
+}
+
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> nums[i];
+    }
+    perm(0);
+    cout << ans;
+    return 0;
+}
+
