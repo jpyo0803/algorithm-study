fd02fe5ef7556fe2111d25325b0da913aecf2287
Jinwon Pyo <gch99517@gmail.com>
2020-05-17 02:45:46 +0900

    Add solution to boj 1766

---
+++ boj2/1766.cpp
@@ -0,0 +1,47 @@
+//
+// Question bank
+// Problem: https://www.acmicpc.net/problem/1766
+//
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+const int MAX{32000};
+int N, M, u, v;
+
+vector<int> adj_list[MAX + 1];
+priority_queue<int, vector<int>, greater<int>> pq;
+int in_degree[MAX + 1];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> N >> M;
+    for (int i = 0; i < M; i++) {
+        cin >> u >> v;
+        adj_list[u].push_back(v);
+        in_degree[v] += 1;
+    }
+
+    for (int i = 1; i <= N; i++) {
+        if (in_degree[i] == 0) {
+            pq.push(i);
+        }
+    }
+
+    while (!pq.empty()) {
+        int u = pq.top(); pq.pop();
+        cout << u << " ";
+        for (auto v : adj_list[u]) {
+            in_degree[v] -= 1;
+            if (in_degree[v] == 0) {
+                pq.push(v);
+            }
+        }
+    }
+    return 0;
+}
+
