c2fcc77656ffc530f752dc9278354402f852882e
Jinwon Pyo <gch99517@gmail.com>
2020-05-22 00:28:12 +0900

    Add O(n) solution to boj 10211

--- boj2/10211.cpp
+++ boj2/10211.cpp
@@ -5,13 +5,16 @@
 
 #include <iostream>
 
+#define MODE 1 // 0 = Divide and conquer = O(nlgn), 1 = linear probing = O(n)
+
 using namespace std;
 
 const int MAX{1000};
 const int NINF{-100000000};
 int T, N;
-int nums[MAX];
 
+#if (MODE == 0)
+int nums[MAX];
 int max_subarr_cross(int arr[], int l, int r) {
     int left_max{NINF}, right_max{NINF};
 
@@ -39,7 +42,20 @@ int max_subarr(int arr[], int l, int r) {
     int mid = (l + r) / 2;
     return max(max(max_subarr(arr, l, mid),  max_subarr(arr, mid + 1, r)), max_subarr_cross(arr, l, r));
 }
-
+#else
+int max_subarr(int n) {
+    int max_sum = NINF;
+    int sum = 0;
+    int e;
+    for (int i = 0; i < n; i++) {
+        cin >> e;
+        sum += e;
+        max_sum = max(max_sum, sum);
+        if (sum < 0) sum = 0;
+    }
+    return max_sum;
+}
+#endif
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
@@ -47,10 +63,14 @@ int main() {
     cin >> T;
     for (int tc = 0; tc < T; tc++) {
         cin >> N;
+#if (MODE == 0)
         for (int i = 0; i < N; i++) {
             cin >> nums[i];
         }
         cout << max_subarr(nums, 0, N - 1) << endl;
+#else
+        cout << max_subarr(N) << endl;
+#endif
     }
     return 0;
 }
