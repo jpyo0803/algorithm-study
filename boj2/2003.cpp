7f7ea879f530e29fffdfab5e71c272938501088a
Jinwon Pyo <gch99517@gmail.com>
2020-05-25 00:53:34 +0900

    Add solution to boj 2003

---
+++ boj2/2003.cpp
@@ -0,0 +1,39 @@
+//
+// Sum of Numbers 2
+// Problem: https://www.acmicpc.net/problem/2003
+//
+
+#include <iostream>
+
+using namespace std;
+
+const int MAX{10000};
+int N, M;
+int nums[MAX];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        cin >> nums[i];
+    }
+
+    int ans{0};
+    int sum{0};
+    int i{0}, j{-1};
+    while (i < N && j < N) {
+        if (sum < M) {
+            sum += nums[++j];
+        } else if (sum > M) {
+            sum -= nums[i++];
+        } else {
+            ans += 1;
+            sum -= nums[i++];
+        }
+    }
+    cout << ans;
+
+    return 0;
+}
