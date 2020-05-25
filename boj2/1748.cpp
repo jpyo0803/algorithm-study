649578d61490e4702a8a71f4ff01c6ec8ce3f490
Jinwon Pyo <gch99517@gmail.com>
2020-05-26 01:40:38 +0900

    Add solution to boj 1748

---
+++ boj2/1748.cpp
@@ -0,0 +1,24 @@
+//
+// Write number continuously 1
+// Problem: https://www.acmicpc.net/problem/1748
+//
+
+#include <iostream>
+
+using namespace std;
+string N;
+int main() {
+    cin >> N;
+
+    int sum{0};
+    int factor{1}, digit{1};
+    for (int i = 1; i < N.length(); i++) {
+        sum += factor * 9 * digit;
+        factor *= 10;
+        digit += 1;
+    }
+    sum += (atoi(N.c_str()) - factor + 1) * digit;
+    cout << sum;
+    return 0;
+}
+
