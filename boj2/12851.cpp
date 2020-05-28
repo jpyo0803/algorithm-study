2f346a2ce372da0468eec6174e21ebeebe9337a8
Jinwon Pyo <gch99517@gmail.com>
2020-05-29 02:15:42 +0900

    Add solution to boj 12851

---
+++ boj2/12851.cpp
@@ -0,0 +1,65 @@
+//
+// Hide and Seek 2
+// Problem: https://www.acmicpc.net/problem/12851
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int MAX{100000};
+const int INF{100000000};
+int N, K;
+int sec, possiblity{0};
+
+int level[MAX + 1];
+
+void bfs(int n, int k) {
+    vector<int> frontier, next;
+    frontier.push_back(n);
+
+    int ans{0};
+    int c_level = 0;
+    bool found{false};
+
+    while (!frontier.empty() && !found) {
+        next.clear();
+        for (auto u : frontier) {
+            if (u == k) {
+                possiblity += 1;
+                sec = c_level;
+                found = true;
+            } else {
+                int v{u - 1};
+                if (v >= 0 && c_level <= level[v]) {
+                    level[v] = c_level;
+                    next.push_back(v);
+                }
+                v = u + 1;
+                if (v <= MAX && c_level <= level[v]) {
+                    level[v] = c_level;
+                    next.push_back(v);
+                }
+                v = u * 2;
+                if (v > 0 && v <= MAX && c_level <= level[v]) {
+                    level[v] = c_level;
+                    next.push_back(v);
+                }
+            }
+        }
+        c_level += 1;
+        swap(frontier, next);
+    }
+}
+
+int main() {
+    cin >> N >> K;
+    for (int i = 0; i <= MAX; i++) {
+        level[i] = INF;
+    }
+    bfs(N, K);
+    cout << sec << endl;
+    cout << possiblity;
+    return 0;
+}
