325f7555c47966102f940ac77bd373273064767c
Jinwon Pyo <gch99517@gmail.com>
2020-06-03 23:30:18 +0900

    Add solution to boj 14881 & 1644

---
+++ boj2/14881.cpp
@@ -0,0 +1,32 @@
+//
+// Waterbottle Problem
+// Problem: https://www.acmicpc.net/problem/14881
+//
+
+#include <cstdio>
+
+using namespace std;
+int T, x, y, z;
+
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+int main() {
+    scanf("%d", &T);
+    for (int tc = 0; tc < T; tc++) {
+        scanf("%d%d%d", &x, &y, &z);
+        int larger = x > y ? x : y;
+        if (larger < z) {
+            printf("NO\n");
+            continue;
+        }
+        int smaller = (x + y) - larger;
+        int v = gcd(larger, smaller);
+        if (z % v == 0) printf("YES\n");
+        else printf("NO\n");
+    }
+    return 0;
+}
+
---
+++ boj2/1644.cpp
@@ -0,0 +1,44 @@
+//
+// Consecutive sum of prime numbers
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+const int MAX{4000000};
+int N;
+bool arr[MAX + 1];
+void erasto(vector<int>& vec, int upto) {
+    for (int i = 2; i <= upto; i++) {
+        if (!arr[i]) {
+            vec.push_back(i);
+            for (int j = i; j <= upto; j += i) {
+                arr[j] = true;
+            }
+        }
+    }
+}
+int main() {
+    cin >> N;
+    vector<int> pn;
+    erasto(pn, N);
+
+    int i{0}, j{-1};
+
+    int count{0}, sum{0};
+    while (i < pn.size() && j < (int)pn.size()) {
+        if (sum < N) {
+            j++;
+            sum += pn[j];
+        } else if (sum > N) {
+            sum -= pn[i];
+            i++;
+        } else {
+            count += 1;
+            j++;
+            sum += pn[j];
+        }
+    }
+    cout << count;
+    return 0;
+}
