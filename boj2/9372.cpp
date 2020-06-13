599d823514b98e9affc01706a29c042fb9598073
Jinwon Pyo <gch99517@gmail.com>
2020-06-13 19:46:52 +0900

    Add solution to boj 9372

--- boj2/9372.cpp
+++ boj2/9372.cpp
@@ -1,4 +1,18 @@
 //
-// Created by pyo on 2020-06-07.
+// Travel of Sangkeun
+// Problem: https://www.acmicpc.net/problem/9372
 //
 
+#include <cstdio>
+int T, N, M, a, b;
+int main() {
+    scanf("%d", &T);
+    for (int tc = 0; tc < T; tc++) {
+        scanf("%d%d", &N, &M);
+        for (int i = 0; i < M; i++) {
+            scanf("%d%d", &a, &b);
+        }
+        printf("%d\n", N - 1);
+    }
+    return 0;
+}
