95a5ed1506373cbed74113cd036603cd7389efd3
Jinwon Pyo <gch99517@gmail.com>
2020-06-24 00:56:30 +0900

    Add solution to boj 1977

---
+++ boj3/1977.cpp
@@ -0,0 +1,27 @@
+//
+// Perfect Square
+// Problem: https://www.acmicpc.net/problem/1977
+//
+
+#include <cstdio>
+#include <cmath>
+int M, N;
+
+int main() {
+    scanf("%d%d", &M, &N);
+    int sidx = ceil(sqrt(M));
+    int sum = 0;
+    int min;
+    bool first{true};
+    for (int i = ceil(sqrt(M)); i <= floor(sqrt(N)); i++) {
+        sum += i * i;
+        if (first) {
+            first = false;
+            min = i * i;
+        }
+    }
+    if (first) printf("-1");
+    else printf("%d\n%d", sum, min);
+    return 0;
+}
+
