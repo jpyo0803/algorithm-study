7060c383d3e8133940d13c0f996bfecf0893dfc1
Jinwon Pyo <gch99517@gmail.com>
2020-06-24 21:02:20 +0900

    Add solution to boj 2010

---
+++ boj3/2010.cpp
@@ -0,0 +1,19 @@
+//
+// Plug
+// Problem: https://www.acmicpc.net/problem/2010
+//
+
+#include <cstdio>
+int N, e;
+int main() {
+    scanf("%d", &N);
+    int sum{-(N - 1)};
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &e);
+        sum += e;
+    }
+    printf("%d", sum);
+
+    return 0;
+}
+
