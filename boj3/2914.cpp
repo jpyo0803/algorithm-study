03474c85933c4c2c1e9c7e71853f4132feaebbf0
Jinwon Pyo <gch99517@gmail.com>
2020-06-28 01:29:57 +0900

    Add solution to boj 2914

---
+++ boj3/2914.cpp
@@ -0,0 +1,13 @@
+//
+// Copyright
+// Problem: https://www.acmicpc.net/problem/2914
+//
+
+#include <cstdio>
+int A, I;
+int main() {
+    scanf("%d%d", &A, &I);
+    printf("%d", A * (I - 1) + 1);
+    return 0;
+}
+
