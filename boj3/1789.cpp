a03ed9206ec1a86c729cddc579e87dab10422d09
Jinwon Pyo <gch99517@gmail.com>
2020-06-28 01:59:31 +0900

    Add solution to boj 1789

---
+++ boj3/1789.cpp
@@ -0,0 +1,15 @@
+//
+// Sum of Numbers
+// Problem: https://www.acmicpc.net/problem/1789
+//
+
+#include <iostream>
+using namespace std;
+uint64_t S;
+int main() {
+    cin >> S;
+    uint64_t N = 1;
+    while (N * (N + 1) / 2 <= S) N++;
+    cout << N - 1;
+    return 0;
+}
