fd2ca215fd875f9f94bc27bc88805cdbbb7781fd
Jinwon Pyo <gch99517@gmail.com>
2020-07-05 03:05:49 +0900

    Add solution to boj 1026

---
+++ boj3/1026.cpp
@@ -0,0 +1,28 @@
+//
+// Treasure
+// Problem: https://www.acmicpc.net/problem/1026
+//
+
+#include <cstdio>
+#include <algorithm>
+const int MAX{50};
+int N;
+int A[MAX];
+int B[MAX];
+
+int main() {
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++) scanf("%d", &A[i]);
+    for (int i = 0; i < N; i++) scanf("%d", &B[i]);
+
+    std::sort(A, A + N);
+    std::sort(B, B + N);
+
+    int sum = 0;
+    for (int i = 0; i < N; i++) {
+        sum += A[i] * B[N - 1 - i];
+    }
+    printf("%d", sum);
+    return 0;
+}
+
