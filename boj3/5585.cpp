b5d0c11bd8ad2b29bf1ce064630b58f99750972d
Jinwon Pyo <gch99517@gmail.com>
2020-07-05 02:58:08 +0900

    Add solution to boj 5585

---
+++ boj3/5585.cpp
@@ -0,0 +1,24 @@
+//
+// Change
+// Problem: https://www.acmicpc.net/problem/5585
+//
+
+#include <cstdio>
+
+int p;
+int coin_type[6] = {500, 100, 50, 10, 5, 1};
+
+int main() {
+    scanf("%d", &p);
+
+    int change_left = 1000 - p;
+    int num_coins = 0;
+    for (int i = 0; i < 6; i++ ){
+        if (change_left >= coin_type[i]) {
+            num_coins += change_left / coin_type[i];
+            change_left %= coin_type[i];
+        }
+    }
+    printf("%d", num_coins);
+}
+
