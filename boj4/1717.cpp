21ff753313af71c511939fa38f084d12c570ed7a
Jinwon Pyo <gch99517@gmail.com>
2020-08-16 14:41:49 +0000

    Add solution to boj 1717

--- boj4/1717.cpp
+++ boj4/1717.cpp
@@ -11,6 +11,7 @@ using namespace std;
 int n, m, a, b, c;
 int parent[kMaxSize + 1];
 
+int count = 0;
 int GetParent(int me) {
   if (parent[me] == me) return me;
   return parent[me] = GetParent(parent[me]);
@@ -18,7 +19,7 @@ int GetParent(int me) {
 
 int main() {
   scanf("%d%d", &n, &m);
-  for (int i = 1; i <= n; i++) parent[i] = i;
+  for (int i = 0; i <= n; i++) parent[i] = i;
   for (int i = 0; i < m; i++) {
     scanf("%d%d%d", &a, &b, &c);
     int rb = GetParent(b);
@@ -29,7 +30,7 @@ int main() {
       else
         printf("NO\n");
     } else {
-      parent[rb] = rc;
+      if (rb != rc) parent[rc] = rb;
     }
   }
   return 0;
