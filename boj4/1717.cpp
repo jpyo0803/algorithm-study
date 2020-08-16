8ae9c7adf1fe73ba4bfba5f04ae24a886ae1b131
Jinwon Pyo <gch99517@gmail.com>
2020-08-16 08:28:08 +0000

    Add solution to boj 1717

---
+++ boj4/1717.cpp
@@ -0,0 +1,36 @@
+//
+// Representation of Set
+// Link: https://www.acmicpc.net/problem/1717
+//
+
+#include <cstdio>
+
+constexpr int kMaxSize = 1000000;
+using namespace std;
+
+int n, m, a, b, c;
+int parent[kMaxSize + 1];
+
+int GetParent(int me) {
+  if (parent[me] == me) return me;
+  return parent[me] = GetParent(parent[me]);
+}
+
+int main() {
+  scanf("%d%d", &n, &m);
+  for (int i = 1; i <= n; i++) parent[i] = i;
+  for (int i = 0; i < m; i++) {
+    scanf("%d%d%d", &a, &b, &c);
+    int rb = GetParent(b);
+    int rc = GetParent(c);
+    if (a) {
+      if (rb == rc)
+        printf("YES\n");
+      else
+        printf("NO\n");
+    } else {
+      parent[rb] = rc;
+    }
+  }
+  return 0;
+}
