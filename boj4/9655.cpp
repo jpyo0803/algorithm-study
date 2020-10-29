bc859c4ecffa8ebd59435c75bad528bcba45c92f
Jinwon Pyo <gch99517@gmail.com>
2020-10-29 15:32:51 +0000

    Add solution to boj 9655

---
+++ boj4/9655.cpp
@@ -0,0 +1,13 @@
+//
+// Stone Game
+// Link: https://www.acmicpc.net/problem/9655
+//
+
+#include <cstdio>
+int N;
+
+int main() {
+  scanf("%d", &N);
+  if (N % 2) printf("SK\n");
+  else printf("CY\n");
+}
