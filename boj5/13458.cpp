ca062d4082a0d85b4b096966951f51119e39a91a
Jinwon Pyo <gch99517@gmail.com>
2020-11-03 17:05:11 +0000

    Add solution to boj 13458

---
+++ boj5/13458.cpp
@@ -0,0 +1,29 @@
+//
+// 시험 감독
+// Link: https://www.acmicpc.net/problem/13458
+//
+
+#include <cstdio>
+#include <cstdint>
+
+int N;
+int A[1000000];
+int B, C;
+
+int main() {
+  scanf("%d", &N);
+  for (int i = 0; i < N; i++) scanf("%d", &A[i]);
+  scanf("%d%d", &B, &C);
+  
+  uint64_t ans = 0;
+  for (int i = 0; i < N; i++) {
+    ans++;
+    A[i] -= B;
+    if (A[i] > 0) {
+      ans += A[i] / C;
+      if (A[i] % C) ans++;
+    }
+  }
+  printf("%llu\n", ans);
+  return 0;
+}
