6b90ce2f16676873245609fbcc4dc344e442da8f
Jinwon Pyo <gch99517@gmail.com>
2021-01-16 19:51:24 +0000

    Add solution to boj 2747

---
+++ boj5/2747.cpp
@@ -0,0 +1,27 @@
+//
+// 피보나치 수
+// Link: https://www.acmicpc.net/problem/2747
+//
+
+#include <cstdio>
+
+using namespace std;
+
+int n;
+
+int cache[46];
+
+int Fibonacci(int x) {
+	if (x < 2) return x;
+	int& ret = cache[x];
+	if (ret != 0) return ret;
+
+	ret = Fibonacci(x - 1) + Fibonacci(x - 2);
+	return ret;
+}
+
+int main() {
+	scanf("%d", &n);
+	printf("%d\n", Fibonacci(n));
+	return 0;
+}	
