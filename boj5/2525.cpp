703e753ee2f00d03a1dd607455b9ebfa45187e2c
Jinwon Pyo <gch99517@gmail.com>
2021-02-03 23:07:23 +0900

    Add solution to boj 2525

---
+++ boj5/2525.cpp
@@ -0,0 +1,30 @@
+//
+// 오븐 시계
+// Link: https://www.acmicpc.net/problem/2525
+//
+
+#include <iostream>
+
+using namespace std;
+
+int hr, minute;
+int elasped_min;
+
+int main() {
+	cin >> hr >> minute >> elasped_min;
+	
+	int comp_hr = elasped_min / 60;
+	int comp_min = elasped_min % 60;
+
+	hr += comp_hr;
+	minute += comp_min;
+
+	if (minute > 59) {
+		hr += 1;
+		minute %= 60;
+	}
+
+	hr %= 24;
+	cout << hr << " " << minute << endl;
+	return 0;
+}
