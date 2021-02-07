1dcfa01dd51c3ba59a935603e5de373aabf21d6f
Jinwon Pyo <gch99517@gmail.com>
2021-02-07 19:31:47 +0900

    Add solution to boj 10422

---
+++ boj5/10422.cpp
@@ -0,0 +1,34 @@
+//
+// 괄호 
+// Link: https://www.acmicpc.net/problem/10422
+//
+
+#include <cstdio>
+#include <cstdint>
+
+constexpr uint64_t kMod = 1000000007;
+int T, L;
+
+uint64_t cache[5001];
+
+uint64_t Catalan(int x) {
+	uint64_t& ret = cache[x];
+	if (ret != 0) return ret;
+	for (int a = 0; a <= x - 2; a += 2) {
+		int b = x - 2 - a;
+		ret += (Catalan(a) * Catalan(b)) % kMod;
+		ret %= kMod;
+	}
+	return ret;
+}
+
+int main() {
+	cache[0] = cache[2] = 1;
+	scanf("%d", &T);
+	for (int t = 0; t < T; t++) {
+		scanf("%d", &L);
+		if (L % 2) printf("0\n");
+		else printf("%llu\n", Catalan(L));
+	}
+	return 0;
+}
