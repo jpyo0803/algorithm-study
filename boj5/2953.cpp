2e8d80cc090f4ca7a77f0438316b7bd75b79e988
Jinwon Pyo <gch99517@gmail.com>
2021-02-10 02:28:09 +0900

    Add solution to boj 2953

---
+++ boj5/2953.cpp
@@ -0,0 +1,25 @@
+//
+// 나는 요리사다
+// Link: https://www.acmicpc.net/problem/2953a
+//
+
+#include <cstdio>
+
+int main() {
+    int max_val = 0;
+    int nth = 1;
+    for (int i = 0; i < 5; i++) {
+        int sum = 0;
+        for (int j = 0; j < 4; j++) {
+            int x;
+            scanf("%d", &x);
+            sum += x;
+        }
+        if (max_val < sum) {
+            max_val = sum;
+            nth = i + 1;
+        }
+    }
+    printf("%d %d\n", nth, max_val);
+    return 0;
+}
