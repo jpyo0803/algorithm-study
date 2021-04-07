6a7cd86e5a48db017ce3186277624c8a4cb34fb1
Jinwon Pyo <gch99517@gmail.com>
2021-04-08 01:13:19 +0900

    Add solution to boj 10867

---
+++ boj5/10867.cpp
@@ -0,0 +1,36 @@
+//
+// 중복빼고 정렬하기
+// Link: https://www.acmicpc.net/problem/10867
+//
+
+#include <stdio.h>
+#include <algorithm>
+
+int Abs(int x) { return x < 0 ? -x : x; }
+
+int N;
+int arr[100000];
+bool was_there[2001];
+
+int main() {
+	scanf("%d", &N);
+	int arr_length = 0;
+	for (int n = 0; n < N; n++) {
+		int x;
+		scanf("%d", &x);
+		if (x < 0) x = Abs(x) + 1000;
+		if (!was_there[x]) {
+			was_there[x] = true;
+			if (x > 1000) x = -x + 1000;
+			arr[arr_length++] = x;
+		}
+	}
+
+	std::sort(arr, arr + arr_length);
+	for (int i = 0; i < arr_length; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+
+	return 0;
+}
