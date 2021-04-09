a893d0d4405470fe8f8da60711e645511d6277ff
Jinwon Pyo <gch99517@gmail.com>
2021-04-10 01:46:12 +0900

    Add solution to boj 11576

---
+++ boj6/11576.cpp
@@ -0,0 +1,34 @@
+//
+// Base Conversion
+// Link: https://www.acmicpc.net/problem/11576
+//
+
+#include <stdio.h>
+#include <vector>
+
+int A, B;
+int num_digit;
+int dec = 0;
+
+int main() {
+	scanf("%d%d%d", &A, &B, &num_digit);
+	int digit;
+	for (int i = num_digit - 1; i >= 0; i--) {
+		scanf("%d", &digit);
+		dec = dec * A + digit;
+	}
+
+	std::vector<int> ans;
+
+	while (dec > 0) {
+		ans.push_back(dec % B);
+		dec /= B;
+	}
+
+	for (auto rev_it = ans.rbegin(); rev_it != ans.rend(); rev_it++) {
+		printf("%d ", *rev_it);
+	}
+	printf("\n");
+
+	return 0;
+}
