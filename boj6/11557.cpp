62a23f1b89513e3fb020299cf216b434a8c169db
Jinwon Pyo <gch99517@gmail.com>
2021-04-11 00:00:47 +0900

    Add solution to boj 11557

---
+++ boj6/11557.cpp
@@ -0,0 +1,30 @@
+//
+// Yangjojang of The Year
+// Link: https://www.acmicpc.net/problem/11557
+//
+
+#include <iostream>
+
+using namespace std;
+
+int T, N;
+
+int main() {
+	cin >> T;
+	for (int t = 0; t < T; t++) {
+		cin >> N;
+		string most_druken;
+		int most_value = 0;
+		for (int n = 0; n < N; n++) {
+			string school;
+			int value;
+			cin >> school >> value;
+			if (value > most_value) {
+				most_value = value;
+				most_druken = school;
+			}
+		}	
+		cout << most_druken << endl;
+	}
+	return 0;
+}
