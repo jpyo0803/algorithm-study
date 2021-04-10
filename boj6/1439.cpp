3f97e9aff5117357cec89747891c0df9d733e91e
Jinwon Pyo <gch99517@gmail.com>
2021-04-10 20:43:38 +0900

    Add solution to boj 1439

---
+++ boj6/1439.cpp
@@ -0,0 +1,32 @@
+//
+// 뒤집기
+// Link: https://www.acmicpc.net/problem/1439
+//
+
+#include <iostream>
+
+using namespace std;
+
+int Solve(string& S) {
+	char pivot = S[0];
+	S.push_back(pivot);
+
+	int diff_count = 0;
+	for (int i = 0; i < S.size();) {
+		if (S[i] != pivot) {
+			diff_count++;
+			while (S[++i] != pivot);
+		} else {
+			i++;
+		}
+	}
+	return diff_count;
+}
+
+int main() {
+	string S;
+	cin >> S;
+	cout << Solve(S) << endl;
+
+	return 0;
+}
