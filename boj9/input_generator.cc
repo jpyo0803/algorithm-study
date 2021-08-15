92fcb3ef0f003e5196e937ec643d227921db525d
Jinwon Pyo <gch99517@gmail.com>
2021-08-15 22:48:01 +0900

    Add random number generator to input_generator

--- boj9/input_generator.cc
+++ boj9/input_generator.cc
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
+#include <cstdlib>
+#include <ctime>
+
 int N;
 
 int main() {
-  N = 100000;
+  N = 10000;
+  srand(time(NULL));
   printf("%d\n", N);
-  for (int i = 99999; i >= 0; i--) {
-    printf("%d %d %d %d\n", i, i, i + 1, i + 1);
+  for (int i = 0; i < N; ++i) {
+    // printf("%d\n", rand() % 1000001a);
+    printf("%d\n", 500000 - i);
   }
   return 0;
 }
