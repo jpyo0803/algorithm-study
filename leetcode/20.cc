6b888d173778e9e0032516b0a68741b04ed8f658
Jinwon Pyo <gch99517@gmail.com>
2021-08-29 13:40:13 +0900

    Add solution to leetcode 20

---
+++ leetcode/20.cc
@@ -0,0 +1,30 @@
+// 20. Valid Parentheses
+// https://leetcode.com/problems/valid-parentheses/
+
+class Solution {
+ public:
+  bool isValid(string s) {
+    stack<char> stk;
+
+    for (auto ch : s) {
+      if (ch == '(' || ch == '[' || ch == '{') {
+        stk.push(ch);
+      } else {
+        char other;
+        if (ch == ')')
+          other = '(';
+        else if (ch == ']')
+          other = '[';
+        else if (ch == '}')
+          other = '{';
+        if (stk.empty() || other != stk.top()) {
+          stk.push(ch);
+          break;
+        }
+        stk.pop();
+      }
+    }
+
+    return stk.empty();
+  }
+};
