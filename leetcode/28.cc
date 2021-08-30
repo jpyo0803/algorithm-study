02cee623499ffcc220ac198f170a9246d09ffdbb
Jinwon Pyo <gch99517@gmail.com>
2021-08-31 00:31:53 +0900

    Add solution to leetcode 28

--- leetcode/28.cc
+++ leetcode/28.cc
@@ -1,50 +1,119 @@
-//
-// Implement strStr()
+// 28. Implement strStr()
 // Problem: https://leetcode.com/problems/implement-strstr/
-//
 
+// Simple Bruteforce
 class Solution {
  public:
   int strStr(string haystack, string needle) {
-    if (needle.length() > haystack.length()) return -1;
-    if (needle.length() == 0) return 0;
-    int n = haystack.length();
-    int m = needle.length();
+    int lh = haystack.size();
+    int ln = needle.size();
+    if (ln == 0) return 0;
 
-    vector<int> pi(m, 0);
-
-    int begin = 1, matched = 0;
-
-    while (begin + matched < m) {
-      if (needle[begin + matched] == needle[matched]) {
-        matched += 1;
-        pi[begin + matched - 1] = matched;
-      } else {
-        if (matched == 0) {
-          begin += 1;
+    int ans = -1;
+    for (int i = 0; i <= lh - ln; ++i) {
+      int match = 0;
+      for (int j = 0; j < ln; ++j) {
+        if (haystack[i + j] == needle[j]) {
+          ++match;
         } else {
-          begin += matched - pi[matched - 1];
-          matched = pi[matched - 1];
+          break;
+        }
+      }
+      if (match == ln) {
+        ans = i;
+        break;
+      }
+    }
+    return ans;
+  }
+};
+
+// Rolling Hash
+class Solution {
+ public:
+  int strStr(string haystack, string needle) {
+    int lh = haystack.size();
+    int ln = needle.size();
+    if (ln == 0) return 0;
+
+    int rhn = 0;
+    for (int i = 0; i < ln; ++i) {
+      rhn += static_cast<int>(needle[i]);
+    }
+
+    int rhh = 0;
+
+    int ans = -1;
+    for (int i = 0; i < lh; ++i) {
+      rhh += static_cast<int>(haystack[i]);
+      if (i < ln - 1) continue;
+
+      if (rhh == rhn) {
+        int match = 0;
+        for (int j = 0; j < ln; ++j) {
+          if (haystack[i - ln + 1 + j] == needle[j]) {
+            ++match;
+          } else {
+            break;
+          }
+        }
+        if (match == ln) {
+          ans = i - ln + 1;
+          break;
+        }
+      }
+      rhh -= static_cast<int>(haystack[i - ln + 1]);
+    }
+    return ans;
+  }
+};
+
+// KMP
+class Solution {
+ public:
+  int strStr(string haystack, string needle) {
+    if (needle.empty()) return 0;
+
+    int ans = -1;
+
+    vector<int> failure_table(needle.size(), 0);
+
+    int b = 1;
+    int m = 0;
+
+    while (b + m < needle.size()) {
+      if (needle[b + m] == needle[m]) {
+        ++m;
+        failure_table[b + m - 1] = m;
+      } else {
+        if (m == 0) {
+          ++b;
+        } else {
+          b += m - failure_table[m - 1];
+          m = failure_table[m - 1];
         }
       }
     }
 
-    begin = matched = 0;
-    while (begin <= n - m) {
-      if (matched < m && haystack[begin + matched] == needle[matched]) {
-        matched += 1;
-        if (matched == m) {
-          return begin;
+    b = m = 0;
+
+    while (b + m <= haystack.size()) {
+      if (m < needle.size() && needle[m] == haystack[b + m]) {
+        ++m;
+        if (m == needle.size()) {
+          ans = b;
+          break;
         }
       } else {
-        if (matched == 0) {
-          begin += 1;
+        if (m == 0) {
+          ++b;
         } else {
-          begin += matched - pi[matched - 1];
-          matched = pi[matched - 1];
+          b += m - failure_table[m - 1];
+          m = failure_table[m - 1];
         }
       }
     }
-    return -1;
+
+    return ans;
   }
 };
