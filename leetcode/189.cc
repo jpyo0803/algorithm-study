7064baf13539b2db90a76dcf0849137727d16dcd
Jinwon Pyo <gch99517@gmail.com>
2021-09-03 00:44:00 +0900

    Add solution to leetcode 189

---
+++ leetcode/189.cc
@@ -0,0 +1,12 @@
+// 189. Rotate Array
+// https://leetcode.com/problems/rotate-array/
+
+class Solution {
+ public:
+  void rotate(vector<int>& nums, int k) {
+    k %= nums.size();
+    reverse(nums.begin(), nums.end() - k);
+    reverse(nums.end() - k, nums.end());
+    reverse(nums.begin(), nums.end());
+  }
+};
