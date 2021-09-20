b0ca50789a03a408375de5689ab41b3b49dd82ad
Jinwon Pyo <gch99517@gmail.com>
2021-09-20 15:45:49 +0900

    Add solution to leetcode 572

---
+++ leetcode/572.cc
@@ -0,0 +1,34 @@
+// 572. Subtree of Another Tree
+// https://leetcode.com/problems/subtree-of-another-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+ public:
+  inline bool IsEqual(TreeNode* a, TreeNode* b) {
+    if (!a && !b) return true;
+    if (!(a && b)) return false;
+    if (a->val != b->val) return false;
+
+    return IsEqual(a->left, b->left) && IsEqual(a->right, b->right);
+  }
+
+  bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+    if (!root) return false;
+
+    if (root->val == subRoot->val) {
+      if (IsEqual(root, subRoot)) return true;
+    }
+    return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+  }
+};
