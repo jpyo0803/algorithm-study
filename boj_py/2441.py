7a5615cc74108e91c42d4cce376473d83d105f5d
Jinwon Pyo <gch99517@gmail.com>
2021-09-20 18:28:02 +0900

    Add solution (python) to boj 2441

---
+++ boj_py/2441.py
@@ -0,0 +1,12 @@
+# 별 찍기 - 4
+# https://www.acmicpc.net/problem/2441
+
+n = int(input())
+
+for i in range(n):
+    for j in range(n):
+        if j < i:
+            print(" ", end='')
+        else:
+            print("*", end='')
+    print(end='\n')
