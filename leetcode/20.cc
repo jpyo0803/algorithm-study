ed7db0c9ec01c83487e27af461da0c666980cd7c
Jinwon Pyo <gch99517@gmail.com>
2021-09-22 10:47:49 +0900

    Add another solution to leetcode 20

--- leetcode/20.cc
+++ leetcode/20.cc
@@ -28,3 +28,28 @@ class Solution {
     return stk.empty();
   }
 };
+
+// Second Try
+class Solution {
+ public:
+  bool isValid(string s) {
+    stack<char> stk;
+
+    for (auto ch : s) {
+      if (ch == '(' || ch == '[' || ch == '{') {
+        stk.push(ch);
+      } else {
+        if (stk.empty()) return false;
+
+        if (ch == ')' && stk.top() != '(')
+          return false;
+        else if (ch == ']' && stk.top() != '[')
+          return false;
+        else if (ch == '}' && stk.top() != '{')
+          return false;
+        stk.pop();
+      }
+    }
+    return stk.empty();
+  }
+};
