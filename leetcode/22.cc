7228b6b1bad9f22ab1cdb3e1a8feb528cff907be
Jinwon Pyo <gch99517@gmail.com>
2021-09-24 22:37:41 +0900

    Add solution to leetcode 22

--- leetcode/22.cc
+++ leetcode/22.cc
@@ -1,6 +1,7 @@
 // 22. Generate Parentheses
 // https://leetcode.com/problems/generate-parentheses/
 
+// First Try
 class Solution {
  public:
   vector<string> strs[9];
@@ -26,4 +27,28 @@ class Solution {
     Solve(n, 1);
     return strs[n];
   }
+};
+
+// Second Try
+class Solution {
+ public:
+  vector<string> Solve(int n) {
+    if (n == 0) return {""};
+    if (n == 1) return {"()"};
+
+    vector<string> ret;
+    for (int i = 0; i < n; ++i) {
+      int j = n - i - 1;
+      auto a = Solve(i);
+      auto b = Solve(j);
+      for (auto& aa : a) {
+        for (auto& bb : b) {
+          ret.push_back("(" + aa + ")" + bb);
+        }
+      }
+    }
+    return ret;
+  }
+
+  vector<string> generateParenthesis(int n) { return Solve(n); }
 };
