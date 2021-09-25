8f1335efd913424f9e194788a5a6fde7efd18ba1
Jinwon Pyo <gch99517@gmail.com>
2021-09-25 13:07:01 +0900

    Add solution to leetcode 226

---
+++ leetcode/226.cc
@@ -0,0 +1,27 @@
+// 226. Invert Binary Tree
+// https://leetcode.com/problems/invert-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+ public:
+  TreeNode* invertTree(TreeNode* root) {
+    if (root == nullptr) return root;
+    auto rl = invertTree(root->left);
+    auto rr = invertTree(root->right);
+
+    root->left = rr;
+    root->right = rl;
+    return root;
+  }
+};
