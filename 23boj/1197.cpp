24aa00fe29ae77f93b0f38cfae8012781f45b148
Jinwon Pyo <gch99517@gmail.com>
2023-05-18 18:36:07 +0900

    Add solution of boj 1197

---
+++ 23boj/1197.cpp
@@ -0,0 +1,27 @@
+// 최소 스패닝 트리 (feat. 피보나치힙)
+// https://www.acmicpc.net/problem/1197
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class FibonacciHeap {
+ public:
+  struct Node {
+    int64_t key, value;
+    bool marked;
+  };
+
+ public:
+  FibonacciHeap() {}
+
+ public:
+};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  return 0;
+}
