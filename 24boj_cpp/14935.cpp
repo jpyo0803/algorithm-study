085efd11cebda1a080aab1e68345b48989fa3f65
Jinwon Pyo <gch99517@gmail.com>
2024-04-19 22:45:14 +0900

    Add solution to boj 14935

---
+++ 24boj_cpp/14935.cpp
@@ -0,0 +1,16 @@
+// FA
+// https://www.acmicpc.net/problem/14935
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  cout << "FA\n";
+
+  return 0;
+}
