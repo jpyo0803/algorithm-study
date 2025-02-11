5f2d0bef8c9f09b6c8f11c1f1b1ede403a5b920c
Jinwon Pyo <jpyo0803@casys.kaist.ac.kr>
2025-02-11 20:13:37 +0900

    Add boj 20922 (two pointer)

--- 25boj_cpp/20922.cpp
+++ 25boj_cpp/20922.cpp
@@ -14,33 +14,24 @@ int main() {
 
   cin >> n >> k;
 
+  vector<int> vv(n);
+  for (int i = 0; i < n; ++i) cin >> vv[i];
+
+  vector<int> cnt(100010, 0);
+  int l, r;
+
+  l = r = 0;
+
   int ans = 0;
 
-  vector<queue<int>> qv(100010);
-
-  int curr_len = 0;
-
-  int last_barrier = -1;
-  for (int i = 0; i < n; ++i) {
-    int x;
-    cin >> x;
-
-    qv[x].push(i);
-
-    if (qv[x].size() > k) {
-      int pos_first = qv[x].front();
-      qv[x].pop();
-      if (last_barrier < pos_first) {
-        curr_len = i - pos_first;
-        last_barrier = pos_first;
-      } else {
-        curr_len++;
-      }
-    } else {
-      curr_len++;
+  while (r < n) {
+    cnt[vv[r]]++;
+    while (cnt[vv[r]] > k) {
+      cnt[vv[l++]]--;
     }
-    // cout << i << " / " << x << " / curr len : " << curr_len << endl;
-    ans = max(ans, curr_len);
+    r++;
+    ans = max(ans, r - l);
+    // cout << l << " / " << r << " : " << ans << endl;
   }
 
   cout << ans << "\n";
