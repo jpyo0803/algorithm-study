870144a66db2f3f7228e1f460b922a51ac28ae66
Jinwon Pyo <gch99517@gmail.com>
2025-05-13 19:58:59 +0900

    Add docker

---
+++ docker/launch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+IMAGE_NAME=my-ubuntu_24.04-env
+
+# 현재 스크립트 기준으로 Git repo 루트 절대경로 계산
+REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+# 빌드 (REPO_ROOT 기준으로 docker 디렉토리 지정)
+docker build -t $IMAGE_NAME "$REPO_ROOT/docker"
+
+# 컨테이너 실행
+docker run -it --rm \
+  -v "$REPO_ROOT":/home/user/algorithm-study \
+  -w /home/user/algorithm-study \
+  $IMAGE_NAME
