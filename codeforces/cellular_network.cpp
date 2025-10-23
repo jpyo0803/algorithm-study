d0bd676303827daf2e196e7d715a901b1b05f399
Jinwon Pyo <gch99517@gmail.com>
2025-10-23 16:57:43 +0900

    Add cf cellular network (binary search)

--- codeforces/cellular_network.cpp
+++ codeforces/cellular_network.cpp
@@ -7,6 +7,19 @@
 
 using namespace std;
 
+int UpperBound(const vector<int>& v, int n, ll x) {
+  int l = 0, r = n;
+  while (l < r) {
+    int mid = (l + r) / 2;
+    if (v[mid] <= x) {
+      l = mid + 1;
+    } else {
+      r = mid;
+    }
+  }
+  return l;
+}
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
@@ -17,15 +30,16 @@ int main() {
   vector<int> av(n), bv(m);
   for (auto& e : av) cin >> e;
   for (auto& e : bv) cin >> e;
+  sort(bv.begin(), bv.end());
 
-  int x = 0, y = 0;
   int ans = 0;
-  while (x < n) {
-    while (y < m && av[x] > bv[y]) y++;
-    int l = y - 1 >= 0 ? abs(av[x] - bv[y - 1]) : 2e9;
-    int r = y < m ? abs(av[x] - bv[y]) : 2e9;
-    ans = max(ans, min(l, r));
-    x++;
+  for (auto e : av) {
+    int pos = UpperBound(bv, m, e);
+    int ldist = 2e9;
+    int rdist = 2e9;
+    if (pos != m) rdist = bv[pos] - e;
+    if (pos != 0) ldist = e - bv[pos - 1];
+    ans = max(ans, min(ldist, rdist));
   }
   cout << ans << "\n";
 
